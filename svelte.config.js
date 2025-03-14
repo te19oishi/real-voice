1e08e5133bcff507fec69deba25a6ded331a7fc0
Hinata Oishi <92960051+te19oishi@users.noreply.github.com>
2025-03-14 20:29:16 +0900

    Switch adapter from @sveltejs/adapter-static to @sveltejs/adapter-auto in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-static';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 import rehypeExternalLinks from 'rehype-external-links';
